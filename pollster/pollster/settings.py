3c0b21fb69330e7ce9dcab44685a39daa00a9e07
NishantRout <nishantnarayanrout@gmail.com>
2021-08-21 15:13:41 +0530

    first commit

--- pollster/pollster/settings.py
+++ pollster/pollster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bhs35%h*3s5ix8f7zuq71!6+t=3f&ze0rtjf8f8m)p-@dy*39a'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
